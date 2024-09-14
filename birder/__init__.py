fa2d88e0d32c83a0dcf423bfc7139c1dec9d88d7
Ofer Hasson <hassonofer@gmail.com>
2024-09-14 21:01:13 +0300

    Bump version: v0.0.5a29 -> v0.0.5a30

--- birder/__init__.py
+++ birder/__init__.py
@@ -4,7 +4,7 @@ from birder.common.lib import get_size_from_signature
 from birder.core.transforms.classification import inference_preset as classification_transform
 from birder.model_registry.model_registry import list_pretrained_models
 
-__version__ = "v0.0.5a29"
+__version__ = "v0.0.5a30"
 
 __all__ = [
     "classification_transform",
