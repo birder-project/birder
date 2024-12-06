29c794d98eba49bfafd0beefc33223fa8737ea07
Ofer Hasson <hassonofer@gmail.com>
2024-12-06 10:28:21 +0200

    Sync from private repo

--- hubconf.py
+++ hubconf.py
@@ -1,6 +1,8 @@
 # pylint: disable=wrong-import-position
 dependencies = ["torch"]
 
+from functools import partial as _partial  # noqa: E402
+
 import torch  # noqa: E402
 
 import birder  # noqa: E402
@@ -10,7 +12,7 @@ import birder.model_registry  # noqa: E402
 def _load(model: str, pretrained: bool = True, input_channels: int = 3, num_classes: int = 0) -> torch.nn.Module:
     dst = torch.hub.get_dir() + f"/{model}.pt"
     if pretrained is True:
-        return birder.load_pretrained_model(model, dst=dst)
+        return birder.load_pretrained_model(model, dst=dst)[0]
 
     model_info = birder.model_registry.registry.get_pretrained_info(model)
     network = model_info["net"]["network"]
@@ -25,6 +27,6 @@ def _load(model: str, pretrained: bool = True, input_channels: int = 3, num_clas
 
 _model_dict = {}
 for m in birder.list_pretrained_models():
-    _model_dict[m.replace("-", "_")] = _load
+    _model_dict[m.replace("-", "_")] = _partial(_load, m)
 
 globals().update(_model_dict)
